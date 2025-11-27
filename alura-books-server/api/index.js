464e36a4815e14f5e6821390247c5717d70621c7
Ana Paula Santos <paulinhabushido1@gmail.com>
2025-11-27 18:23:45 -0300

    fix: CORS detalhado para liberar a comunicacao com o Frontend

--- alura-books-server/api/index.js
+++ alura-books-server/api/index.js
@@ -6,7 +6,11 @@ const cors = require("cors")
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 app.use("/livros", rotaLivro);
 app.use("/favoritos", rotaFavorito);
